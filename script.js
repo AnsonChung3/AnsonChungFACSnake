f033390e5efef4b5e8ba0f399277349f6675e39c
Anson <AnsonChung3@gmail.com>
2025-08-31 14:31:56 +0100

    Add method to generate and draw food. Tested.

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const GRID_SIZE = 20;
 const GRID_COUNT = 20;
 
-const gameCanvas = document.getElementById('gamegameCanvas');
+const gameCanvas = document.getElementById('gameCanvas');
 const ctx = gameCanvas.getContext('2d');
 gameCanvas.width = GRID_SIZE * GRID_COUNT;
 gameCanvas.height = GRID_SIZE * GRID_COUNT;
 
 ctx.fillStyle = '#da5151ff';
 ctx.fillRect(0, 0, 50, gameCanvas.height);
+
+// food generation
+let food = {}
+
+function generateFood() {
+    food.x = Math.floor(Math.random() * GRID_COUNT);
+    food.y = Math.floor(Math.random() * GRID_COUNT);
+}
+
+function drawFood() {
+    ctx.fillStyle = '#DB826E';
+    ctx.fillRect(food.x * GRID_SIZE, food.y * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+}
+
+function testAutoFood() {
+    setInterval(() => {
+        generateFood();
+        drawFood();
+    }, 2000);
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     testAutoFood();
+// });
